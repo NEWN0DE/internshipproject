4ff5ea8b457ca4475f587ddedcef3177be1f47e2
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2022-10-07 09:45:33 +0300

    .NET Core’da AutoMapper Kullanımı Gerçekleştirildi.

---
+++ CountryCity/Mapping/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CountryCity.Models;
+using CountryCity.Models.ViewModel;
+using CountryCity.ViewModel;
+
+namespace CountryCity.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AppUserViewModel, AppUser>();
+
+            CreateMap<AppUser,AppUserViewModel>();
+
+            CreateMap<RoleViewModel,AppRole>();
+
+            CreateMap<AppRole,RoleViewModel>();
+
+            CreateMap<CountryViewModel,Country>();
+
+            CreateMap<Country,CountryViewModel>();     
+            
+            
+                        
+
+            
+        }
+
+    }
+}
