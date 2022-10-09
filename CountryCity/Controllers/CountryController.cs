497ae3cbb1d5dadfd9fcdb83d85b0be7786074ba
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2022-10-09 20:14:00 +0300

    MongoDb implementasyonu ve Crud işlemlerinin bir kısmı yapıldı

---
+++ CountryCity/Controllers/CountryController.cs
@@ -0,0 +1,43 @@
+﻿using CountryCity.Models.MongoDB;
+using CountryCity.Models.MongoDB.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryCity.Controllers
+{
+    public class CountryController : Controller
+    {
+        //Buradaki Crud işlemleri MongoDb ile yapılacaktır.
+       
+        private readonly CountriesService countriesService;
+
+        public CountryController(CountriesService countriesService)
+        {
+            this.countriesService = countriesService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var country = await countriesService.GetAsync();
+
+
+            return View(country);
+
+        }
+
+        public async Task<IActionResult> AddCountry()
+        {
+            return View();
+           
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCountry(CountryMongoDB C)
+        {
+            await countriesService.CreateAsync(C);
+
+            return RedirectToAction("Get", "Country");
+
+        }
+
+    }
+}
